dccebcafa9575a14088404331b874cc269bd29dd
李先营 <298151501@qq.com>
2019-01-14 14:51:42 +0800

    新建类

---
+++ src/com/system/entity/SqlGoods.java
@@ -0,0 +1,283 @@
+package com.system.entity;
+
+public class SqlGoods {
+    private String goods_id;
+    private Integer platform_id;
+    private String seller_id;
+    private String goods_title;
+    private String goods_stitle;
+    private String goods_pic;
+    private String goods_url;
+    private String goods_intro;
+    private Double goods_price;
+    private Integer goods_sale;
+    private Integer goods_cid;
+    private Double commission_rate;
+    private Integer commission_type;
+    private String coupon_id;
+    private String coupon_url;
+    private Double coupon_price;
+    private Double after_coupon;
+    private String coupon_condition;
+    private Integer coupon_total;
+    private Integer coupon_rest;
+    private Integer coupon_use;
+    private String coupon_start;
+    private String coupon_end;
+    private Integer is_tmall;
+    private Integer is_ju;
+    private Integer is_qiang;
+    private Integer is_yun;
+    private Integer is_gold;
+    private Integer is_ji;
+    private Integer is_hai;
+    private Double dsr;
+
+    public String getGoods_id() {
+        return goods_id;
+    }
+
+    public void setGoods_id(String goods_id) {
+        this.goods_id = goods_id;
+    }
+
+    public Integer getPlatform_id() {
+        return platform_id;
+    }
+
+    public void setPlatform_id(Integer platform_id) {
+        this.platform_id = platform_id;
+    }
+
+    public String getSeller_id() {
+        return seller_id;
+    }
+
+    public void setSeller_id(String seller_id) {
+        this.seller_id = seller_id;
+    }
+
+    public String getGoods_title() {
+        return goods_title;
+    }
+
+    public void setGoods_title(String goods_title) {
+        this.goods_title = goods_title;
+    }
+
+    public String getGoods_stitle() {
+        return goods_stitle;
+    }
+
+    public void setGoods_stitle(String goods_stitle) {
+        this.goods_stitle = goods_stitle;
+    }
+
+    public String getGoods_pic() {
+        return goods_pic;
+    }
+
+    public void setGoods_pic(String goods_pic) {
+        this.goods_pic = goods_pic;
+    }
+
+    public String getGoods_url() {
+        return goods_url;
+    }
+
+    public void setGoods_url(String goods_url) {
+        this.goods_url = goods_url;
+    }
+
+    public String getGoods_intro() {
+        return goods_intro;
+    }
+
+    public void setGoods_intro(String goods_intro) {
+        this.goods_intro = goods_intro;
+    }
+
+    public Double getGoods_price() {
+        return goods_price;
+    }
+
+    public void setGoods_price(Double goods_price) {
+        this.goods_price = goods_price;
+    }
+
+    public Integer getGoods_sale() {
+        return goods_sale;
+    }
+
+    public void setGoods_sale(Integer goods_sale) {
+        this.goods_sale = goods_sale;
+    }
+
+    public Integer getGoods_cid() {
+        return goods_cid;
+    }
+
+    public void setGoods_cid(Integer goods_cid) {
+        this.goods_cid = goods_cid;
+    }
+
+    public Double getCommission_rate() {
+        return commission_rate;
+    }
+
+    public void setCommission_rate(Double commission_rate) {
+        this.commission_rate = commission_rate;
+    }
+
+    public Integer getCommission_type() {
+        return commission_type;
+    }
+
+    public void setCommission_type(Integer commission_type) {
+        this.commission_type = commission_type;
+    }
+
+    public String getCoupon_id() {
+        return coupon_id;
+    }
+
+    public void setCoupon_id(String coupon_id) {
+        this.coupon_id = coupon_id;
+    }
+
+    public String getCoupon_url() {
+        return coupon_url;
+    }
+
+    public void setCoupon_url(String coupon_url) {
+        this.coupon_url = coupon_url;
+    }
+
+    public Double getCoupon_price() {
+        return coupon_price;
+    }
+
+    public void setCoupon_price(Double coupon_price) {
+        this.coupon_price = coupon_price;
+    }
+
+    public Double getAfter_coupon() {
+        return after_coupon;
+    }
+
+    public void setAfter_coupon(Double after_coupon) {
+        this.after_coupon = after_coupon;
+    }
+
+    public String getCoupon_condition() {
+        return coupon_condition;
+    }
+
+    public void setCoupon_condition(String coupon_condition) {
+        this.coupon_condition = coupon_condition;
+    }
+
+    public Integer getCoupon_total() {
+        return coupon_total;
+    }
+
+    public void setCoupon_total(Integer coupon_total) {
+        this.coupon_total = coupon_total;
+    }
+
+    public Integer getCoupon_rest() {
+        return coupon_rest;
+    }
+
+    public void setCoupon_rest(Integer coupon_rest) {
+        this.coupon_rest = coupon_rest;
+    }
+
+    public Integer getCoupon_use() {
+        return coupon_use;
+    }
+
+    public void setCoupon_use(Integer coupon_use) {
+        this.coupon_use = coupon_use;
+    }
+
+    public String getCoupon_start() {
+        return coupon_start;
+    }
+
+    public void setCoupon_start(String coupon_start) {
+        this.coupon_start = coupon_start;
+    }
+
+    public String getCoupon_end() {
+        return coupon_end;
+    }
+
+    public void setCoupon_end(String coupon_end) {
+        this.coupon_end = coupon_end;
+    }
+
+    public Integer getIs_tmall() {
+        return is_tmall;
+    }
+
+    public void setIs_tmall(Integer is_tmall) {
+        this.is_tmall = is_tmall;
+    }
+
+    public Integer getIs_ju() {
+        return is_ju;
+    }
+
+    public void setIs_ju(Integer is_ju) {
+        this.is_ju = is_ju;
+    }
+
+    public Integer getIs_qiang() {
+        return is_qiang;
+    }
+
+    public void setIs_qiang(Integer is_qiang) {
+        this.is_qiang = is_qiang;
+    }
+
+    public Integer getIs_yun() {
+        return is_yun;
+    }
+
+    public void setIs_yun(Integer is_yun) {
+        this.is_yun = is_yun;
+    }
+
+    public Integer getIs_gold() {
+        return is_gold;
+    }
+
+    public void setIs_gold(Integer is_gold) {
+        this.is_gold = is_gold;
+    }
+
+    public Integer getIs_ji() {
+        return is_ji;
+    }
+
+    public void setIs_ji(Integer is_ji) {
+        this.is_ji = is_ji;
+    }
+
+    public Integer getIs_hai() {
+        return is_hai;
+    }
+
+    public void setIs_hai(Integer is_hai) {
+        this.is_hai = is_hai;
+    }
+
+    public Double getDsr() {
+        return dsr;
+    }
+
+    public void setDsr(Double dsr) {
+        this.dsr = dsr;
+    }
+}
