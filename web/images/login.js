41f8fcf2e6e3467277883f1c3167fea3e74eab20
李先营 <298151501@qq.com>
2019-01-16 16:09:27 +0800

    实现登录

--- web/images/login.js
+++ web/images/login.js
@@ -29,10 +29,12 @@ function initLoginBox() {
         },
         methods: {
             login: function () {
-                console.log(hex_md5("taoAdmin"));
-                console.log('login');
+                if (this.checkCanLogin()) {
+                    var temp_psd = hex_md5(this.password);
+                    ajaxLogin(this.username, temp_psd);
+                }
             },
-            checkInputIsEmpty: function () {
+            checkCanLogin: function () {
                 if (this.username == '' || this.username == null) {
                     this.user_tip.txt = '用户名不能为空';
                     this.user_tip.is_show = true;
@@ -45,6 +47,13 @@ function initLoginBox() {
                     return true;
                 }
                 return false;
+            },
+            // 用户名或密码错误提示
+            userOrPsdError: function () {
+                this.user_tip.txt = '请检查您的用户名';
+                this.user_tip.is_show = true;
+                this.psd_tip.txt = '请检查您的密码';
+                this.psd_tip.is_show = true;
             },
             hideErrorTip: function (str) {
                 if (str == 'user') {
@@ -90,20 +99,33 @@ function initLoginBox() {
     // Vue app    end
 }
 
-function ajaxLogin() {
+// 登录
+function ajaxLogin(user, psd) {
     axios({
         url: base_url + '/login',
         method: 'post',
         params: {
-            user: '123',
-            psd: '123'
+            user: user,
+            psd: psd
         }
     }).then(function (response) {
-        if (response != null) {}
+        if (response.data.is_login) {
+            createCookie(user, psd);
+        } else {
+            js_login_box.userOrPsdError();
+        }
         console.log(response);
     }).catch(function (error) {
         console.log(error);
     });
 }
 
-ajaxLogin();
+// 设置cookie
+function createCookie(user, psd) {
+    Cookies.set('taoAssistant', {
+        username: user,
+        password: psd
+    }, {
+        expires: 1
+    });
+}
