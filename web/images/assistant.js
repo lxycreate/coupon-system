0737801efbfd0d4d1ae706eee0b22d2f6dd3a078
李先营 <298151501@qq.com>
2019-01-17 18:39:06 +0800

    新增按钮

--- web/images/assistant.js
+++ web/images/assistant.js
@@ -14,8 +14,10 @@ window.onload = function () {
 // 初始化姓名和密码
 function initUserAndPsd() {
     var temp = Cookies.getJSON('taoAssistant');
-    username = temp.username;
-    password = temp.password;
+    if (temp != undefined) {
+        username = temp.username;
+        password = temp.password;
+    } else {}
     //这里应该删除Cookie,开发过程为了方便先不删
 }
 
@@ -42,6 +44,22 @@ function initContent() {
                 icon_class: {
                     'icon-database': true
                 }
+            }],
+            filter_btns: [{
+                    name: '全部',
+                    is_select: true
+                },
+                {
+                    name: "更新日志",
+                    is_select: false
+                },
+                {
+                    name: "清理日志",
+                    is_select: false
+                }
+            ],
+            sort_btns: [{
+                name: "时间"
             }]
         }
     });
