859a4901ace894ca1b0dbc49d9c88e2be5662ad9
李先营 <298151501@qq.com>
2019-01-18 20:55:47 +0800

    新建淘客助手的Timer类

---
+++ src/com/system/timer/TkzsTimer.java
@@ -0,0 +1,27 @@
+package com.system.timer;
+
+import com.system.update.UpdateGoodsData;
+
+import java.util.TimerTask;
+
+public class TkzsTimer extends TimerTask {
+    private UpdateGoodsData goods_data;
+
+    public TkzsTimer(UpdateGoodsData goods) {
+        this.goods_data = goods;
+    }
+
+    //定时任务
+    @Override
+    public void run() {
+        goods_data.runGetData();
+        //没有更多数据可以读取
+        if (goods_data.getStatusCode() == "none") {
+            this.cancel();
+        }
+    }
+
+    public void setGoods_data(UpdateGoodsData goods) {
+        this.goods_data = goods;
+    }
+}
