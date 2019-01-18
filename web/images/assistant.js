448effe7ea5c0b444e1271ba10222e8532550f2d
李先营 <298151501@qq.com>
2019-01-18 14:09:17 +0800

    实现新建更新任务弹窗

--- web/images/assistant.js
+++ web/images/assistant.js
@@ -3,6 +3,9 @@ var base_url = 'http://localhost:8066';
 // 用户名和密码
 var username = '';
 var password = '';
+
+// 作为获取日志的ajax参数
+var log_obj = {};
 // 内容主体
 var js_main_container;
 // 初始化
@@ -26,6 +29,7 @@ function initContent() {
     js_main_container = new Vue({
         el: '.js_main_container',
         data: {
+            // 左侧按钮   start
             btns: [{
                 name: '数据管理',
                 is_select: true,
@@ -45,6 +49,8 @@ function initContent() {
                     'icon-database': true
                 }
             }],
+            // 左侧按钮   end
+            // "数据管理"下的按钮   start
             filter_btns: [{
                     name: '全部',
                     is_select: true
@@ -58,9 +64,13 @@ function initContent() {
                     is_select: false
                 }
             ],
+            // "数据管理"下的按钮   end
+            // "数据管理"下的排序按钮   start
             sort_btns: [{
                 name: "时间"
             }],
+            // "数据管理"下的排序按钮   end
+            // 更新任务弹窗   start
             update_origin: [{
                     name: '全部',
                     index: 0,
@@ -76,14 +86,35 @@ function initContent() {
                     is_select: false
                 }
             ],
-            is_clean: false
+            is_clean: false,
+            is_show_update_box: false
+            // 更新任务弹窗   end
         },
         // data  end
         methods: {
             test: function (index) {
                 console.log('111');
                 this.update_origin[index].is_select = true;
+            },
+            showUpdateBox: function () {
+                if (!this.is_show_update_box) {
+                    this.is_show_update_box = true;
+                }
+                console.log('showUpdateBox');
+            },
+            hideUpdateBox: function () {
+                if (this.is_show_update_box) {
+                    this.is_show_update_box = false;
+                }
+            },
+            cancelUpdate: function () {
+                this.hideUpdateBox();
+                this.is_clean = false;
+                for (var i = 0; i < this.update_origin.length; ++i) {
+                    this.update_origin[i].is_select = false;
+                }
             }
         }
+        // 
     });
 }
