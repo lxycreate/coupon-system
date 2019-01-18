8fcb810cae1a05dfa7f65ea8deca8bbd516ff633
李先营 <298151501@qq.com>
2019-01-18 19:37:36 +0800

    动态sql语句

--- src/com/system/dao/GoodsDao.java
+++ src/com/system/dao/GoodsDao.java
@@ -1,4 +1,9 @@
 package com.system.dao;
 
+import com.system.entity.SqlGoods;
+
 public interface GoodsDao {
+    public void updateGoodsData(SqlGoods goods);
+    public void insertGoodsData(SqlGoods goods);
+    public Integer checkGoodsExist(String goods_id);
 }
